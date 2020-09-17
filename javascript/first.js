968a126281bda9e1684e7b212f4edc08a332c263
bbajpai <b.bhumi101@gmail.com>
2020-09-17 16:25:55 +0530

    final commit

---
+++ javascript/first.js
@@ -0,0 +1,26 @@
+Es5
+Es6
+Ecma Script 5
+Ecma Script 6
+
+transpile 
+
+//es5
+function add(a,b){
+    return a,b
+}
+
+//es6
+var add = (a,b) => {return a+b}
+
+const a = 10
+
+
+
+////ternory
+var a = 10
+a>10?"hiii":"bie"
+"bie"
+var a = 10
+a==10?"hiii":"bie"
+"hiii"
---
+++ redux2/src/component/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <footer>
+            <div>
+                <hr/>
+                &copy; DeveloperFunnels
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
